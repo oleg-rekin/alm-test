394b66f62380854103e68c19e8cb7bd7db3c472d
Oleg Rekin <oleg.rekin@gmail.com>
2022-02-12 20:24:40 +0300

    EL-2022: Simple locking implemented with some caveats

--- entitylocker/src/main/kotlin/me/orekin/entitylocker/EntityLocker.kt
+++ entitylocker/src/main/kotlin/me/orekin/entitylocker/EntityLocker.kt
@@ -1,13 +1,18 @@
 package me.orekin.entitylocker
 
+import java.util.Queue
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.Semaphore
+import java.util.concurrent.locks.ReentrantLock
 
-class EntityLocker<ID> {
+class EntityLocker<ID : Any> {
 
     private val lockedEntityIds = ConcurrentHashMap.newKeySet<ID>()
 
-    private val waitingRequestsByEntityId = ConcurrentHashMap<ID, ConcurrentLinkedQueue<Boolean>>()
+    private val waitingQueuesByEntityId = ConcurrentHashMap<ID, Queue<Semaphore>>()
+
+    private val waitingQueueLock = ReentrantLock()
 
     fun <R> executeLocked(entityId: ID, protectedBlock: () -> R): R {
         lock(entityId)
@@ -22,11 +27,50 @@ class EntityLocker<ID> {
         if (lockedEntityIds.add(entityId)) {
             return
         }
-        TODO()
+        // Another thread can release the last lock here, TODO recheck
+        val semaphoreToLockOn = addLockedToQueue(entityId)
+        semaphoreToLockOn.acquire()
     }
 
     private fun unlock(entityId: ID) {
-        TODO()
-        lockedEntityIds.remove(entityId)
+        val semaphoreToFree = getNextLockedFromQueueIfExists(entityId)
+        if (semaphoreToFree == null) {
+            lockedEntityIds.remove(entityId)
+            return
+        }
+        semaphoreToFree.release()
+    }
+
+    private fun addLockedToQueue(entityId: ID): Semaphore {
+        waitingQueueLock.lock()
+        try {
+            val waitingQueue = waitingQueuesByEntityId.computeIfAbsent(entityId) { ConcurrentLinkedQueue() }
+            val semaphore = Semaphore(0)
+            waitingQueue.add(semaphore)
+            return semaphore
+        } finally {
+            waitingQueueLock.unlock()
+        }
+    }
+
+    private fun getNextLockedFromQueueIfExists(entityId: ID): Semaphore? {
+        waitingQueueLock.lock()
+        try {
+            val waitingQueue = waitingQueuesByEntityId[entityId] ?: return null
+
+            if (waitingQueue.isEmpty()) {
+                // Should never happen, TODO use proper logging
+                println("Empty queue was not removed for entity ID $entityId, removing it now")
+                waitingQueuesByEntityId.remove(entityId)
+            }
+
+            val semaphore = waitingQueue.remove()
+            if (waitingQueue.isEmpty()) {
+                waitingQueuesByEntityId.remove(entityId)
+            }
+            return semaphore
+        } finally {
+            waitingQueueLock.unlock()
+        }
     }
 }
